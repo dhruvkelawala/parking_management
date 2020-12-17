5e616ce26ec676a71d8809add6b6ad21bff57d6d
Harshal Maniya <harshalmaniya26@gmail.com>
2020-12-17 09:05:10 +0530

    init

---
+++ lib/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'dart:async';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DatabaseReference databaseReference;
+
+  @override
+  void initState() {
+    super.initState();
+    getSensorValue();
+  }
+
+  Future getSensorValue() async {
+    databaseReference =
+        FirebaseDatabase.instance.reference().child("automation-product");
+
+    DataSnapshot dataSnapshot = await databaseReference.once();
+    print(dataSnapshot.value);
+
+    print(dataSnapshot.value["12121212"]);
+
+    return dataSnapshot.value['slot2'];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getSensorValue(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          print(snapshot.data);
+          return Container(
+            child: Column(),
+          ); // Add your Custom Widget of Column, Rows here.
+        }
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
